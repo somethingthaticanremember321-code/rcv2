58d5ab59e038d723aad3107f9f86bb8c35283899
somethingthaticanremember321-code <somethingthaticanremember321-code@users.noreply.github.com>
2026-09-19 21:20:21 +0300

    Phase 0 verified: All 6 data layer unit tests passing with zero warnings

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' hide Category;
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
